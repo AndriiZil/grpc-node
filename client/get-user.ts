ef5822631d29b7b3ad32399bfef2345cbd1766db
Andrii Zilnyk <andrii.zilnyk@railsbank.com>
2021-04-30 13:04:14 +0300

    initial commit

---
+++ client/get-user.ts
@@ -0,0 +1,16 @@
+import { User, UserRequest } from '../proto/users_pb';
+import { client } from './utils';
+
+export default function getUsers(id: number) {
+  return new Promise<User>((resolve, reject) => {
+    const request = new UserRequest();
+    request.setId(id);
+
+    client.getUser(request, (err, user) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(user);
+    });
+  });
+}
